c23ceb6d5765ef271c5bfad2aad78aca32e87f43
Jasper Janboy P. Monterey <janboym@gmail.com>
2018-02-02 13:58:15 +0800

    Add files via upload

---
+++ MoreThanHalfThesis/Models/Icon.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Media.Imaging;
+
+namespace MoreThanHalfThesis.Models
+{
+    public class Icon
+    {
+        public string IconPath { get; set; }
+    }
+}
---
+++ MoreThanHalfThesis/Models/Person.cs
@@ -0,0 +1,37 @@
+﻿using SQLite.Net.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Media.Imaging;
+
+namespace MoreThanHalfThesis.Models
+{
+    class Person
+    {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Age { get; set; }
+        public string Gender { get; set; }
+        public string AvatarPath { get; set; }
+        public string CreationDate { get; set; }
+
+        public Person()
+        {
+
+        }
+
+        public Person(string name, string age, string gender, string avatar)
+        {
+            Name = name;
+            Age = age;
+            Gender = gender;
+            AvatarPath = avatar;
+            CreationDate = DateTime.Now.ToString();
+            
+        }
+
+    }
+}
